40f0d5c25221678dadfc86671a7f6c767e5b975f
qianjiachun <43806319+qianjiachun@users.noreply.github.com>
2019-09-03 22:01:51 +0800

    Add files via upload

---
+++ js/randomNum.js
@@ -0,0 +1,15 @@
+// 用于生成一个范围的整数(>1)
+function randomNum(minNum,maxNum){
+    // 取随机数
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
